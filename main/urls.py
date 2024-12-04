a7b75d91dfd94eccbba9b62604b5f9e6149919e2
secretparisiens <maypozorbe1@gmail.com>
2024-12-04 14:18:44 +0100

    urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path
 from . import views
 from django.shortcuts import redirect
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    # Перенаправление на страницу логина
     path('', lambda request: redirect('log', permanent=False)),
+
+    # Основные маршруты
     path('about', views.about, name='about'),
     path('todolist', views.todolist, name='todolist'),
     path('finance', views.finance, name='finance'),
-    path('reg', views.reg, name='reg'),
-    path('log', views.log, name='log'),
-    path('acc', views.acc, name='acc'),
     path('home', views.home, name='home'),
-    path('log', LoginView.as_view(template_name='log.html'), name='log'),
+    path('acc', views.acc, name='acc'),
+
+    # Регистрация и логин/логаут
+    path('reg', views.user_register, name='reg'),  # Обработчик регистрации
+    path('log', LoginView.as_view(template_name='main/log.html'), name='log'),  # Класс LoginView
+    path('logout', LogoutView.as_view(next_page='log'), name='logout'),  # Логаут
+]
+
     ]
