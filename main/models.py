b48c17aba67bde2924ad88348899cc8b2213feb6
secretparisiens <maypozorbe1@gmail.com>
2024-12-06 07:19:20 +0100

    models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=True)
+    about = models.TextField(blank=True)
+
+# Создаем профиль автоматически при создании пользователя
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
