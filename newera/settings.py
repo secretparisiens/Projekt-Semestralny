ad8cf8d4264587c9b415ce04016ac69d5c683839
secretparisiens <maypozorbe1@gmail.com>
2024-12-06 07:02:56 +0100

    settings.py

--- newera/settings.py
+++ newera/settings.py
@@ -124,3 +124,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Перенаправление после успешного входа
+LOGIN_REDIRECT_URL = '/acc'  # Перенаправление на страницу аккаунта
+
+# Перенаправление после выхода
+LOGOUT_REDIRECT_URL = '/log'  # Перенаправление на страницу логина
+
