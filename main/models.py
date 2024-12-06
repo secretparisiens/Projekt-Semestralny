576dfdb97b645d4e4099eb4df436c263cfd350f4
secretparisiens <maypozorbe1@gmail.com>
2024-12-06 07:41:12 +0100

    models.py

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
-from django.db import models
+from django.db import models import post_save
+from django.dispatch import receiver from django.db.models.signals
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -23,3 +24,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Профиль {self.user.username}"
+        
+        @receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
